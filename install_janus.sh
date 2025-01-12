0887d8e9899fcdd8d8c39fd9d385e553aac95c1b
ManfredAabye <zainhofer@gmx.de>
2025-01-12 12:01:06 +0100

    Update install_janus.sh

--- install_janus.sh
+++ install_janus.sh
@@ -12,14 +12,41 @@ sh autogen.sh
 make
 sudo make install
 
-# Plugins installieren und konfigurieren
-cd ..
-git clone https://github.com/meetecho/janus-plugin-echo.git
-cd janus-plugin-echo || exit
-sh autogen.sh
-./configure --prefix=/opt/janus
-make
-sudo make install
-
 # Janus starten
 sudo /opt/janus/bin/janus
+
+echo "
+Um den Janus-Server zu stoppen und neu zu starten, kannst du die folgenden Befehle verwenden:
+
+# Janus-Server stoppen
+
+sudo systemctl stop janus
+
+
+# Janus-Server neu starten
+
+sudo systemctl start janus
+
+
+# Janus-Server konfigurieren
+
+Um den Janus-Server optimal mit dem Projekt os-webrtc-janus zu konfigurieren, 
+solltest du die Konfigurationsdateien anpassen und sicherstellen, dass die erforderlichen Plugins und Einstellungen korrekt eingerichtet sind.
+
+Hier sind einige Schritte, die du befolgen kannst:
+
+Konfigurationsdateien anpassen: Bearbeite die Konfigurationsdateien in /opt/janus/etc/janus/, um die gewünschten Einstellungen vorzunehmen. Zum Beispiel:
+
+nano /opt/janus/etc/janus/janus.cfg
+
+
+Plugins aktivieren: Stelle sicher, dass die benötigten Plugins in der Konfigurationsdatei aktiviert sind. Zum Beispiel:
+
+nano /opt/janus/etc/janus/janus.plugin.echotest.jcfg
+
+
+HTTPS konfigurieren: Wenn du HTTPS verwenden möchtest, musst du die SSL-Zertifikate konfigurieren und die entsprechenden Einstellungen in der Konfigurationsdatei vornehmen.
+
+Dokumentation lesen: Überprüfe die Dokumentation beider Projekte, um sicherzustellen, dass alle Schritte korrekt befolgt werden. 
+Die Dokumentation von Janus findest du auf GitHub: github.com/meetecho/janus-gateway."
+
